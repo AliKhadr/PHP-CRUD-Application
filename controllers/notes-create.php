5fc5fb93e27ad8a42f3787731fb6e1b193656800
AliKhadr <ali.khadr98@hotmail.com>
2024-06-22 18:16:56 +0300

    Added functionality to the create note page. Added some validation to the rendering of a note

--- controllers/notes-create.php
+++ controllers/notes-create.php
@@ -1,5 +1,19 @@
 <?php
 
 $heading = "Create Notes";
+$currentUser = 1;
+
+$config = require 'config.php';
+$db = new Database($config['database']);
+
+// dd($_SERVER);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // dd($_POST);
+    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => $currentUser
+    ]);
+}
 
 require "views/notes-create.view.php";
