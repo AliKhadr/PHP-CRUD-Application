2fc0a38fc622e4f43f6cef24b15f3f5957802764
AliKhadr <ali.khadr98@hotmail.com>
2024-06-22 20:54:52 +0300

    Added validation to form. Checks if body is empty and character limit

--- controllers/notes-create.php
+++ controllers/notes-create.php
@@ -9,11 +9,22 @@ $db = new Database($config['database']);
 // dd($_SERVER);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // dd($_POST);
-    $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
-        'body' => $_POST['body'],
-        'user_id' => $currentUser
-    ]);
+    $errors = [];
+
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'No body text found...';
+    } 
+
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters';
+    }
+
+    if (empty($errors)) {
+        $db->query('INSERT INTO notes(body, user_id) VALUES (:body, :user_id)', [
+            'body' => $_POST['body'],
+            'user_id' => $currentUser
+        ]);
+    } 
 }
 
 require "views/notes-create.view.php";
--- views/notes-create.view.php
+++ views/notes-create.view.php
@@ -11,8 +11,11 @@
             <div class="col-span-full">
             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Add a note</label>
             <div class="mt-2">
-                <textarea id="body" name="body" rows="3" placeholder="Write something..." class="block w-full rounded-md border-0 p-5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                <textarea id="body" name="body" rows="3" placeholder="Write something..." class="block w-full rounded-md border-0 p-5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"><?= isset($_POST['body']) ? $_POST['body'] : NULL ?></textarea>
             </div>
+                <?php if (isset($errors['body'])) : ?>
+                    <p class="text-red-500 text-s mt-5 ml-5"><?= $errors['body'] ?></p>
+                <?php endif ?>
             </div>      
         </div>
         </div>
