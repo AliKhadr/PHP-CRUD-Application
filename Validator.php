d7657ea8860a46d0b83998a95390e655dc865275
AliKhadr <ali.khadr98@hotmail.com>
2024-07-02 21:56:19 +0300

    Added a Validator class for calidation of form

---
+++ Validator.php
@@ -0,0 +1,13 @@
+<?php
+
+class Validator {
+
+    public static function bodyText($text, $min = 1, $max = INF) {
+
+        $value = trim($text);
+
+        return strlen($value) >= $min && strlen($value) <= $max;
+    }
+
+
+}
--- controllers/note.php
+++ controllers/note.php
@@ -6,6 +6,12 @@ $currentUser = 1;
 $config = require 'config.php';
 $db = new Database($config['database']); 
 
+
+// if (! $_GET){
+//     abort(404);
+// }
+
+
 $note = $db->query('select * from notes where id = :id', ['id'=> $_GET['id']])->findOrFail();
 
 authorize($note['user_id'] == $currentUser);
